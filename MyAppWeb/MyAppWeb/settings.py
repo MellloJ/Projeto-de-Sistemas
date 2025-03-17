f0df48a92bca579635ed19ebe07645f82a3ae8f3
MellloJ <jdesousamadeira@gmail.com>
2025-03-17 12:14:50 -0300

    Criacao da documentacao da api de autenticacao

--- MyAppWeb/MyAppWeb/settings.py
+++ MyAppWeb/MyAppWeb/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'corsheaders', #permite requisições Flutter
     "rest_framework_simplejwt.token_blacklist",
+    "drf_yasg",
 ]
 
 MIDDLEWARE = [

22fb6fda992975f056119eced565091ae01de815
CaesarCrew <joao.moraes2@estudante.ifto.edu.br>
2025-04-01 00:29:51 -0100

    You shall not pass

--- MyAppWeb/users/services/signupValidation.py
+++ MyAppWeb/users/services/signupValidation.py
@@ -1,8 +1,10 @@
 from django import forms
 
 class signupForm(forms.Form):
-    email
-    password
-    cpf
-    name = Form.CharField(max_lenght="50")
-    lastName = Form.
+    email = forms.EmailField()
+    password =
+    cpf = forms.IntegerField()
+    name = forms.CharField(max_lenght="50")
+    lastName = forms.CharField()
+
+def 
--- MyAppWeb/users/views.py
+++ MyAppWeb/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from users.services import googleLogin
-from 
+from .signupValidation import signupForm
 
 # Create your views here.
 
@@ -11,23 +11,13 @@ class Login(View):
 
 
 class Signup(View):
+    form = signupForm()
     def get(self, request):
-        return render(request, "signup/signup.html")
+        return render(request, "signup/signup.html", {'form': form})
 
-    def formHandler(request):
-    # if this is a POST request we need to process the form data
-    if request.method == "POST":
-        # create a form instance and populate it with data from the request:
-        form = NameForm(request.POST)
-        # check whether it's valid:
+    def post(request):
+        form = signupForm(request.POST)
+        # if this is a POST request we need to process the form data
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
+
             return HttpResponseRedirect("/thanks/")
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = NameForm()
-
-    return render(request, "name.html", {"form": form})

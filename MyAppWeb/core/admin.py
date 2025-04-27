5aed82a57e6f6e2681f7fc884e12b173c76f0db9
Jesse Silva <jesse1eliseu@gmail.com>
2025-04-27 18:26:48 -0300

    feat: Add SVG illustration and update navbar template
    - Added a new SVG illustration in moto.html for visual enhancement.
    - Created a responsive navbar in navbar.html with user greeting and authentication links.
    - Updated the Index view to pass categories to the template context.
    - Modified styles.css to define a new dark base color variable.
    - Commented out the color-changing script in scripts.js for future use.
    - Removed empty migrations directory in users app.
    - Cleaned up unused import in users/views.py.

--- MyAppWeb/core/admin.py
+++ MyAppWeb/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.apps import apps
+from django.conf import settings
 
-# Register your models here.
+MY_APPS = settings.MY_APPS
+
+for app_name in MY_APPS:
+    models = apps.get_app_config(app_name).get_models()
+    for model in models:
+        admin.site.register(model)
--- MyAppWeb/core/views.py
+++ MyAppWeb/core/views.py
@@ -1,7 +1,12 @@
 from django_dump_die.middleware import dd
 from django.shortcuts import render
 from django.views import View
+from core.consts import CATEGORIAS_ALIMENTOS
 
 class Index(View):
     def get(self, request):
-        return render(request, "index.html")
+        context = {
+            'title': 'Traz Aí | Home',
+            'categories': CATEGORIAS_ALIMENTOS,
+        }
+        return render(request, "index.html",context)

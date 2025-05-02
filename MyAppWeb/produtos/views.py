8051ca8427b1f6f742ff1ea4a70c96b53d10ec41
Jesse Silva <jesse1eliseu@gmail.com>
2025-05-02 17:51:45 -0300

    feat: Add side filter form and update homepage URL
    - Created a new side filter form in sidefilter.html for product filtering options including categories, price range, and ratings.
    - Updated the URL configuration to set the homepage path to the root URL instead of '/home/'.
    - Modified the Index view to include categories from CATEGORIAS_ALIMENTOS in the context for rendering the products page.

--- MyAppWeb/produtos/views.py
+++ MyAppWeb/produtos/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.views import View
 from produtos.models import Produtos
+from core.consts import CATEGORIAS_ALIMENTOS
 
 class Index(View):
     def get(self, request):
         context = {
             'title': 'Traz Aí | Produtos',
             'produtos' : Produtos.objects.all(),
+            'categories' : CATEGORIAS_ALIMENTOS,
+
         }
         return render(request, "produtos_index.html",context)

8866846fa921eb346e1f5d8fc0e3139a97541251
MellloJ <jdesousamadeira@gmail.com>
2025-05-11 22:11:10 -0300

    Finalizacao da api de pedidos

--- MyAppWeb/pedidos/serializers.py
+++ MyAppWeb/pedidos/serializers.py
@@ -4,34 +4,33 @@ from .models import Pedido, ItemPedido, DadosEntrega
 class ItemPedidoSerializer(serializers.ModelSerializer):
     class Meta:
         model = ItemPedido
-        fields = '__all__'
+        exclude = ['pedido_id']  # Não exigimos isso do usuário
 
 class DadosEntregaSerializer(serializers.ModelSerializer):
     class Meta:
         model = DadosEntrega
-        fields = '__all__'
+        exclude = ['pedido_id']  # Também será atribuído automaticamente
 
 class PedidoSerializer(serializers.ModelSerializer):
     itens = ItemPedidoSerializer(many=True, required=False)
-    dados_entrega = DadosEntregaSerializer(many=True, required=False, source='dados_entrega')
+    dados_entrega = DadosEntregaSerializer(many=True, required=False)
 
     class Meta:
         model = Pedido
         fields = '__all__'
 
-        def create(self, validated_data):
-            itens_data = validated_data.pop('itens', [])
-            entrega_data = validated_data.pop('dados_entrega', [])
-            pedido = Pedido.objects.create(**validated_data)
+    def create(self, validated_data):
+        itens_data = validated_data.pop('itens', [])
+        entrega_data = validated_data.pop('dados_entrega', [])
+        pedido = Pedido.objects.create(**validated_data)
 
-            for item in itens_data:
-                ItemPedido.objects.create(pedido_id=pedido, **item)
+        for item in itens_data:
+            ItemPedido.objects.create(pedido_id=pedido, **item)
 
-            for entrega in entrega_data:
-                DadosEntrega.objects.create(pedido_id=pedido, **entrega)
+        for entrega in entrega_data:
+            DadosEntrega.objects.create(pedido_id=pedido, **entrega)
 
-            return pedido
-
+        return pedido
 
     def update(self, instance, validated_data):
         for attr, value in validated_data.items():
--- MyAppWeb/pedidos/urls.py
+++ MyAppWeb/pedidos/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('pedido/create', PedidoCreateView.as_view()),                              # POST
-    path('pedido/', PedidoListView.as_view()),                                      # GET (todos)
-    path('pedido/<int:id>', PedidoRetrieveUpdateDeleteView.as_view()),             # GET, PATCH, DELETE
-    path('pedido/<str:numeroPedido>/numeroPedido', PedidoByNumeroPedidoView.as_view()),  # GET (por número)
-    path('pedido/<int:id>/cancel', PedidoCancelView.as_view()),                     # DELETE (cancelamento)
-    path('pedido/<int:id>/statusPagamento', AtualizaStatusPagamentoView.as_view()), # PATCH (status)
+    path('create', PedidoCreateView.as_view()),                              # POST
+    path('', PedidoListView.as_view()),                                      # GET (todos)
+    path('<int:id>', PedidoRetrieveUpdateDeleteView.as_view()),             # GET, PATCH, DELETE
+    path('<str:numeroPedido>/numeroPedido', PedidoByNumeroPedidoView.as_view()),  # GET (por número)
+    path('<int:id>/cancel', PedidoCancelView.as_view()),                     # DELETE (cancelamento)
+    path('<int:id>/statusPagamento', AtualizaStatusPagamentoView.as_view()), # PATCH (status)
 ]

e3f4fa1f0d4e88a768f49a0b661a1481e0e2c1c3
CaesarCrew <joao.moraes2@estudante.ifto.edu.br>
2025-05-24 19:06:32 -0300

    Criação das APIs de gerenciamento do endereço do usuário

--- MyAppWeb/users/urls.py
+++ MyAppWeb/users/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
 
     path('users/addresses/', AddressCreateView.as_view(), name='address-create'),
     path('users/addresses/delete/<int:pk>', AddressDeleteView.as_view(), name='address-delete'),
+    path('users/addresses/edit/<int:pk>', AddressEditView.as_view(), name='address-edit'),
+    path('users/addresses/get/all', AddressListView.as_view(), name='address-get-all'),
+    path('users/addresses/get/<int:pk>', AddressGetView.as_view(), name='address-get-one'),
 ]
